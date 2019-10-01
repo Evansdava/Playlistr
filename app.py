5d84edfc4cf6fd3c7fc626df242d1be158f089de
David Evans <evansdav.a@gmail.com>
2019-10-01 14:56:53 -0700

    added created_at, footer, and bootstrap theme

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from unittest import TestCase, main as unittest_main
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
 import os
 
 
@@ -32,7 +33,8 @@ def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+    return render_template('playlists_show.html', playlist=playlist,
+                           comments=playlist_comments)
 
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
@@ -55,8 +57,10 @@ def playlists_submit():
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
+        'videos': request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
+    print(playlist)
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
@@ -86,7 +90,17 @@ def comments_new():
     }
     print(comment)
     comment_id = comments.insert_one(comment).inserted_id
-    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+    return redirect(url_for('playlists_show',
+                            playlist_id=request.form.get('playlist_id')))
+
+
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show',
+                    playlist_id=comment.get('playlist_id')))
 
 
 if __name__ == '__main__':
